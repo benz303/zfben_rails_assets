6e3986e11ec9fccaefa19dcc237ef48d6d48e3d3
ben <ben@zfben.(none)>
2011-11-18 10:21:21 +0800

    use rails engine

--- lib/zfben_rails_assets.rb
+++ lib/zfben_rails_assets.rb
@@ -1,14 +1,7 @@
 ZfbenRailsAssetsPath = File.join File.dirname(__FILE__), 'zfben_rails_assets'
 
 module ZfbenRailsAssets
-  class Railtie < Rails::Railtie
-    railtie_name :zfben_rails_assets
-      
-    rake_tasks do
-      load File.join(ZfbenRailsAssetsPath, 'tasks.rb')
-    end
-    Rails.application.routes.draw do
-      get '/test' => 'test'
-    end
+  class Engine < Rails::Engine
+    
   end
 end
